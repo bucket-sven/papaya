f7eb5817a695d23c68ed76a585fa3e1577e5a958
sven <sven199109@gmail.com>
2018-02-02 23:40:47 +0800

    add css

--- src/main/kotlin/com/sunshine/papaya/controller/MainController.kt
+++ src/main/kotlin/com/sunshine/papaya/controller/MainController.kt
@@ -1,13 +1,25 @@
 package com.sunshine.papaya.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
+import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import javax.servlet.http.HttpServletRequest
 
 @Controller
 class MainController {
+    val logger = LoggerFactory.getLogger(MainController::class.java)
+
     @RequestMapping("/", method = [ RequestMethod.GET ])
-    fun home(): String {
+    fun home(map: ModelMap, request: HttpServletRequest): String {
+//        var session = request.session
+//        var key = session.getAttribute("key")
+//        logger.info("key: {}", key)
+//        if (key == null) {
+//            session.setAttribute("key", "spring-boot")
+//        }
+        map.addAttribute("data", "This is Data!!!")
         return "index"
     }
 }
